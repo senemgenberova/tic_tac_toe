8c41f7d18da54ab7fe25708da87882149fb87003
P301 <P301>
2017-05-26 21:12:41 +0400

    tictactoe

---
+++ tic tac toe/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace tic_tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.player = new System.Windows.Forms.TextBox();
+            this.reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            this.button1.Click += fillButton;
+            this.button2.Click += fillButton;
+            this.button3.Click += fillButton;
+            this.button4.Click += fillButton;
+            this.button5.Click += fillButton;
+            this.button6.Click += fillButton;
+            this.button7.Click += fillButton;
+            this.button8.Click += fillButton;
+            this.button9.Click += fillButton;
+
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.Location = new System.Drawing.Point(12, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 47);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 47);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(213, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 47);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 95);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 41);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(113, 95);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 41);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(213, 95);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 41);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 168);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(74, 43);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(113, 168);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(71, 43);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(213, 168);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 43);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 252);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(87, 25);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Turn";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // player
+            // 
+            this.player.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.player.Location = new System.Drawing.Point(12, 273);
+            this.player.Multiline = true;
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(87, 30);
+            this.player.TabIndex = 10;
+            this.player.Text = "player1";
+            this.player.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(124, 252);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(89, 34);
+            this.reset.TabIndex = 11;
+            this.reset.Text = "reset game";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 353);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox player;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ tic tac toe/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac_toe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool playerChoosing()
+        {
+            if (this.player.Text == "player1")
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void fillButton(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (this.playerChoosing())
+            {
+                b.Text = "X";
+                this.player.Text = "player2";
+
+            }
+            else {
+                b.Text = "O";
+                this.player.Text = "player1";
+            }
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            this.button1.Text = "";
+            this.button2.Text = "";
+            this.button3.Text = "";
+            this.button4.Text = "";
+            this.button5.Text = "";
+            this.button6.Text = "";
+            this.button7.Text = "";
+            this.button8.Text = "";
+            this.button9.Text = "";
+        }
+    }
+}
